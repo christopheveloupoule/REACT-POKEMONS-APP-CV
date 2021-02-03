1d7561c4957536af3fcb55d0eb610f32243437e1
Christophe <christophevelou@gmail.com>
2021-02-03 23:04:50 +0100

    Creation de pokemon-list

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -2,8 +2,7 @@ import React, { FunctionComponent } from 'react';
 import Pokemon from '../models/pokemon';
 
 type Props = { //On def un nouveau type pr typescript
-    pokemon: Pokemon,
-    borderColor?: string // to TS, variable is faculatif (borderColor?)
+    pokemon: Pokemon
   };
   
 const PokemonCard: FunctionComponent<Props> = ({pokemon}) => {
---
+++ src/pages/pokemon-list.tsx
@@ -0,0 +1,28 @@
+import React, { FunctionComponent, useState, useEffect } from 'react';
+import Pokemon from '../models/pokemon';
+import POKEMONS from '../models/mock-pokemon';
+import PokemonCard from '../components/pokemon-card';
+
+const PokemonList: FunctionComponent = () => {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  //we Initialize our state component with an empty array per default
+  
+  useEffect(() => {
+      setPokemons(POKEMONS);
+  }, []);
+  
+  return (
+    <div>
+      <h1 className="center">Pokédex</h1>
+      <div className="container"> 
+        <div className="row"> 
+        {pokemons.map(pokemon => (
+          <PokemonCard key={pokemon.id} pokemon={pokemon} />
+        ))}
+        </div>
+      </div>
+    </div> 
+  );
+}
+  
+export default PokemonList;
