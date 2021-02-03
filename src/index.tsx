0dbe224c868abbe58595a7f624c86aef885a1ca1
Christophe <christophevelou@gmail.com>
2021-02-03 20:37:08 +0100

    creation du fichier index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+//ajout de 3 imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render( //METHOD render qui prend 2 parametres
+    <App />,
+    document.getElementById('root')
+);
